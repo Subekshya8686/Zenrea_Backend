6f8a62be9652c4a6f85e28066f59121d999c6344
Subekshya8686 <subekshyakayastha4@gmail.com>
2024-01-30 10:19:12 +0545

    New Project Start: Backend

---
+++ src/main/java/com/example/zenrea_backend/ZenreaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.zenrea_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZenreaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZenreaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/zenrea_backend/ZenreaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zenrea_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZenreaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
